8db04239ab5503d4bd8afe02b7efc7cde90206ce
trueenglishcoder <79080325+trueenglishcoder@users.noreply.github.com>
2023-04-13 11:09:51 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import numpy as np
+from statsmodels.stats.weightstats import ttest_ind
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 346373029 # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(x: np.array) -> bool:
+    y = np.empty(len(x))
+    y.fill(500)
+    _, pval, _ = ttest_ind(x, y, alternative = 'larger')
+    return pval < 0.1
